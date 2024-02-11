f73c928aa5568e4de44c440cda5659e01a7a261c
cceph <96626515+CCeph@users.noreply.github.com>
2024-02-11 16:16:59 -0400

    Position homepage/nav elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import { useParams } from "react-router-dom";
 import Homepage from "./Components/Homepage";
 import Shop from "./Components/Shop";
+import Nav from "./Components/Nav";
 
 function App() {
   const {name} = useParams();
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
+    <div className="flex h-screen flex-col items-center">
+      <Nav />
       {name === "shop" ? (
         <Shop />
       ) : (
         <Homepage />
       )}
-      <h1 className="text-3xl">
-      Vite + React + Vitest + React Router + Proptypes + Prettier + Tailwind
-    </h1>
-    <h2 className="text-2xl">Enjoy!</h2>
     </div>
   );
 }
