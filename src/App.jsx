74dbe7f45f29ba09fa5a4d47509d4e72c5d118bf
CCeph <96626515+CCeph@users.noreply.github.com>
2024-02-11 14:57:56 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="text-3xl">
+        Vite + React + Vitest + React Router + Proptypes + Prettier + Tailwind
+      </h1>
+      <h2 className="text-2xl">Enjoy!</h2>
+    </div>
+  );
+}
+
+export default App;
