c3ff81ed290800ce8c127f49f0084375c03c253f
cceph <96626515+CCeph@users.noreply.github.com>
2024-02-12 12:13:08 -0400

    Create commented homepage title text

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Homepage() {
   return (
-    <div className="w-full flex-1 bg-[url('./assets/homepage-banner.jpg')] bg-cover bg-center bg-no-repeat"></div>
+    <div className="flex w-full flex-1 flex-col items-center justify-start bg-[url('./assets/homepage-banner.jpg')] bg-cover bg-center p-5">
+      {/*      <div className="flex flex-col items-center rounded-xl border border-black bg-slate-200 p-3">
+        <h1 className="font-anton p-1 text-6xl">
+          Fake<span className="font-arvo">.</span>
+        </h1>
+        <h2 className="font-arvo p-1 text-4xl">It&apos;s In Our Name</h2>
+        <h3 className="font-arvo p-1 text-2xl font-bold underline">
+          <Link to={"/shop"}>Shop Now</Link>
+        </h3>
+  </div> */}
+    </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        anton: ["Anton", "sans-serif"],
+        arvo: ["Arvo", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
