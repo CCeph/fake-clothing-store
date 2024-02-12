152a5eae04fa44651c1055c4785e937a687a0787
cceph <96626515+CCeph@users.noreply.github.com>
2024-02-12 02:36:35 -0400

    Change how the nav buttons fill nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,20 +3,20 @@ import logo from "../assets/urban.png";
 
 export default function Nav() {
   return (
-    <nav className="flex w-full items-center justify-between justify-items-center bg-black text-white">
+    <nav className="flex w-full items-stretch justify-between justify-items-center bg-black text-white">
       <img src={logo} className="h-24 w-24" />
       <ul className="flex justify-items-center">
-        <Link to={"/"} className="p-9 hover:bg-gray-800">
+        <Link to={"/"} className="flex items-center p-8 hover:bg-gray-800">
           Home
         </Link>
-        <Link to={"/shop"} className="p-9 hover:bg-gray-800">
+        <Link to={"/shop"} className="flex items-center p-8 hover:bg-gray-800">
           Mens
         </Link>
-        <Link to={"/shop"} className="p-9 hover:bg-gray-800">
+        <Link to={"/shop"} className="flex items-center p-8 hover:bg-gray-800">
           Women
         </Link>
       </ul>
-      <h1 className="p-7">Cart</h1>
+      <h1 className="flex items-center p-7">Cart</h1>
     </nav>
   );
 }
