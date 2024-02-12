1b93107ecc47664b2536ca56f20992005ad05bde
cceph <96626515+CCeph@users.noreply.github.com>
2024-02-12 02:06:51 -0400

    Style nav bar page links

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,7 +1,5 @@
 export default function Homepage() {
   return (
-    <div className="w-full flex-1 bg-[url('./assets/homepage-banner.jpg')] bg-cover bg-center bg-no-repeat">
-      <h1>Test</h1>
-    </div>
+    <div className="w-full flex-1 bg-[url('./assets/homepage-banner.jpg')] bg-cover bg-center bg-no-repeat"></div>
   );
 }
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,3 +1,21 @@
+import { Link } from "react-router-dom";
+
 export default function Nav() {
-  return <nav>Nav bar go here</nav>;
+  return (
+    <nav className="flex w-full items-center justify-between justify-items-center bg-black text-white">
+      <h1 className="p-7">Logo</h1>
+      <ul className="flex justify-items-center">
+        <Link to={"/"} className="p-7 hover:bg-gray-800">
+          Home
+        </Link>
+        <Link to={"/shop"} className="p-7 hover:bg-gray-800">
+          Mens
+        </Link>
+        <Link to={"/shop"} className="p-7 hover:bg-gray-800">
+          Women
+        </Link>
+      </ul>
+      <h1 className="p-7">Cart</h1>
+    </nav>
+  );
 }
