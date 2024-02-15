2768ba2f1d5f48f34faf66a29f86cd6da67b82b1
cceph <96626515+CCeph@users.noreply.github.com>
2024-02-14 21:38:47 -0400

    Update Card snapshot

--- src/tests/Card.test.jsx
+++ src/tests/Card.test.jsx
@@ -2,9 +2,10 @@ import { render, screen } from "@testing-library/react";
 import Card from "../Components/Card";
 import { expect } from "vitest";
 import { MemoryRouter } from "react-router-dom";
+import logo from "../assets/urban.png";
 
 it("renders card properly", () => {
-  const image = "fakeurl";
+  const image = logo;
   const price = 0;
   const name = "name";
   const type = "typeName";
