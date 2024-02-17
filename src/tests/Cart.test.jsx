41b0019239a511756e63a6b1d34395fb6a4665ad
cceph <96626515+CCeph@users.noreply.github.com>
2024-02-16 20:56:43 -0400

    Add test titles for checking out

--- src/tests/Cart.test.jsx
+++ src/tests/Cart.test.jsx
@@ -7,3 +7,9 @@ it.skip("Test that the items are added to the cart properly");
 it.skip("Test that items are removed from the cart properly");
 
 it.skip("Test that items count properly when they are added multiple times");
+
+it.skip("Checking out shows the right message in cart");
+
+it.skip("Clearing cart removes checkout message");
+
+it.skip("Adding item removes checkout message");
