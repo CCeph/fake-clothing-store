b32a920f2387b9af995e7fc42296e271973a150b
cceph <96626515+CCeph@users.noreply.github.com>
2024-02-16 20:31:28 -0400

    Remove screen dependency from Card test (not used)

--- src/tests/Card.test.jsx
+++ src/tests/Card.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Card from "../Components/Card";
 import { expect } from "vitest";
 import { MemoryRouter } from "react-router-dom";
