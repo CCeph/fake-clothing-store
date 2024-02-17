d5e64a97600a3dfd1463c5bf96b3affe4ca0b4ab
cceph <96626515+CCeph@users.noreply.github.com>
2024-02-16 20:31:11 -0400

    Add test titles as a reminder

---
+++ src/tests/Cart.test.jsx
@@ -0,0 +1,9 @@
+it.skip("Test that the clear button clears the cart");
+
+it.skip("Test that the total counts properly");
+
+it.skip("Test that the items are added to the cart properly");
+
+it.skip("Test that items are removed from the cart properly");
+
+it.skip("Test that items count properly when they are added multiple times");
